f80f20c00ef91a09e24941b7991494e161100e78
류지혜/Jihye Ryu <51522587+Domwis-IR@users.noreply.github.com>
2024-10-08 19:02:03 +0900

    Title: 문자열의 길이 순, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 241008/문자열의 길이 순/string-length-order.py
+++ 241008/문자열의 길이 순/string-length-order.py
@@ -5,7 +5,7 @@ for _ in range(n):
     word = input()
     words.append((len(word), word))
 
-words.sort(key=lambda words: words[0])
+words.sort(key=lambda words: (words[0], words[1]))
 
 for num, word in words:
     print(word)
