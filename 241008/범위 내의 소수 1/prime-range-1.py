b89959ab0106e3ac6a241df74ee050d950f763bb
류지혜/Jihye Ryu <51522587+Domwis-IR@users.noreply.github.com>
2024-10-08 19:13:57 +0900

    Title: 범위 내의 소수 1, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241008/범위 내의 소수 1/prime-range-1.py
@@ -0,0 +1,21 @@
+import math
+a, b = map(int, input().split())
+
+sum_prime = 0
+min_prime = 10e6
+
+def is_prime(x):
+    for i in range(2, int(math.sqrt(x))+ 1):
+        if x % i == 0:
+            return False
+    return True
+
+for num in range(a, b+1):
+    if num == 1:
+        continue
+    if is_prime(num):
+        sum_prime += num
+        min_prime = min(min_prime, num)
+
+print(sum_prime, end=' ')
+print(min_prime)
