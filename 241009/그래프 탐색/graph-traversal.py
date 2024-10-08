f4a5ec8a4106c1bc5c7f64254974dea4b78fde99
류지혜/Jihye Ryu <51522587+Domwis-IR@users.noreply.github.com>
2024-10-09 00:04:59 +0900

    Title: 그래프 탐색, Time: 266ms, Memory: 26MB, Status: Passed - CodeTree

--- 241009/그래프 탐색/graph-traversal.py
+++ 241009/그래프 탐색/graph-traversal.py
@@ -1,24 +1,34 @@
-n, m = map(int, input().split())
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
 
-graph = [[] for _ in range(n+1)]
+#index를 1번 부터 사용하기 위해 m+1만큼 할당합니다.
+graph = [[] for _ in range(n + 1)]
 
-for _ in range(m):
-    x, y = map(int, input().split())
-    graph[x].append(y)
-    graph[y].append(x)
+visited = [False for _ in range(n + 1)]
+vertex_cnt = 0
 
-visited = [False] * (n+1)
 
-def dfs(current_idx):
-    visited[current_idx] = True
-    count = 1  # 현재 노드를 방문했으므로 초기값을 1로 설정합니다.
+def dfs(vertex):
+    global vertex_cnt
+    
+    # 해당 정점에서 이어져있는 모든 정점을 탐색해줍니다.
+    for curr_v in graph[vertex]:
+        # 아직 간선이 존재하고 방문한 적이 없는 정점에 대해서만 탐색을 진행합니다.
+        if not visited[curr_v]:
+            visited[curr_v] = True
+            vertex_cnt += 1
+            dfs(curr_v)
+    
+    
+for i in range(m):
+    v1, v2 = tuple(map(int, input().split()))
 
-    for link in graph[current_idx]:
-        if not visited[link]:
-            count += dfs(link)  # 연결된 노드를 방문하고 반환된 값을 누적합니다.
+    # 각 정점이 서로 이동이 가능한 양방향 그래프이기 때문에
+    # 각 정점에 대한 간선을 각각 저장해줍니다.
+    graph[v1].append(v2)
+    graph[v2].append(v1)
 
-    return count
+visited[1] = True
+dfs(1)
 
-# 1번 노드와 연결된 모든 노드의 수를 찾음
-connected_count = dfs(1)
-print(connected_count)
+print(vertex_cnt)
