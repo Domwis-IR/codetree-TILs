0d990937677112f99d6130358adb427ea9d82448
류지혜/Jihye Ryu <51522587+Domwis-IR@users.noreply.github.com>
2024-10-09 23:45:44 +0900

    Title: 양수 직사각형의 최대 크기, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241009/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 241009/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -2,6 +2,51 @@ n, m = map(int, input().split())
 
 grid = [list(map(int,input().split())) for _ in range(n)]
 
+def in_range(y,x,n,m):
+    return 0 <= y < n and 0 <= x < m
+
+def get_positive(y,x, n, m, r, c):
+    directions = [(0,1), (1,0), (0,-1),(-1,0)]
+    move_nums = [r, c, r, c]
+
+    is_positive = False
+
+    if r == 0:
+        for _ in range(c):
+            y, x = y + 0, x + 1
+            if in_range(y,x,n,m) and grid[y][x] > 0:
+                is_positive = True
+            else:
+                is_positive = False
+                break
+
+    elif c == 0:
+        for _ in range(r):
+            y, x = y + 1, x + 0
+            if in_range(y,x,n,m) and grid[y][x] > 0:
+                is_positive = True
+            else:
+                is_positive = False
+                break
+
+    else:
+        for (dy, dx), move_num in zip(directions, move_nums):
+            for _ in range(move_num):
+                y, x = y + dy, x + dx
+                if in_range(y,x,n,m) and grid[y][x] > 0:
+                    is_positive = True
+                else:
+                    is_positive = False
+                    break
+            if not is_positive:
+                break
+
+    if is_positive:
+        return (r + 1) * (c + 1)
+    else:
+        return -1
+
+
 max_size = -1
 
 # 모든 좌표에 대해서 
@@ -12,21 +57,8 @@ for i in range(n):
             directions = [(0,1), (1,0), (0,-1),(-1,0)]
             for r in range(n):
                 for c in range(m):
-                    length = [r, c, r, c]
-                    y, x = i, j
-                    for (dy, dx), k in zip(directions, length):
-                        for _ in range(k):
-                            ny, nx = y + dy, x + dx
-                            print("now", ny, nx,k)
-                            if (0<= ny <n and 0 <= nx < m) and grid[ny][nx] > 0:
-                                y, x = ny, nx
-                            else:
-                                length = [-1,-1,-1,-1]
-                                break
-                        if length[0] == -1:
-                            break
-                    size = (length[0]+1) * (length[1]+1)
+                    size = get_positive(i,j,n,m,r,c)
                     max_size = max(max_size, size)
-                    print(i, j, r, c, size, "max_size", max_size)
+                    # print(i, j, r, c, size, "max_size", max_size)
 
-print(max_size if max_size != 0 else -1)
+print(max_size)
