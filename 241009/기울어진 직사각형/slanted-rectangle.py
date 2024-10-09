2c2a0191998074553a2107f212d932d041919301
류지혜/Jihye Ryu <51522587+Domwis-IR@users.noreply.github.com>
2024-10-09 23:12:52 +0900

    Title: 기울어진 직사각형, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241009/기울어진 직사각형/slanted-rectangle.py
@@ -0,0 +1,31 @@
+n = int(input())
+
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+def in_range(y, x, n):
+    return 0 <= y < n and 0 <= x < n
+
+def get_score(y, x, r, c):
+    directions = [(-1,1), (-1,-1), (1, -1), (1,1)]
+    move_nums = [r, c, r, c]
+
+    sum_of_nums = 0
+
+    for (dx, dy) move_num in zip(directions, move_nums):
+        for _ in range(move_num):
+            y, x = y + dy, x + dx
+            if not in_range(y, x, n):
+                return 0
+            sum_of_nums += grid[y][x]
+    
+    return sum_of_nums
+
+ans = 0
+
+for i in range(n):
+    for j in range(n):
+        for r in range(1,n):
+            for c in range(1,n):
+                ans = max(ans, get_score(i, j, r, c))
+
+print(ans)
