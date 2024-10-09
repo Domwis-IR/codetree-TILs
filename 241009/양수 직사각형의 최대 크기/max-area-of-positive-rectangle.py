894454429c11d795e82a55d5728bf4c675096662
류지혜/Jihye Ryu <51522587+Domwis-IR@users.noreply.github.com>
2024-10-09 21:19:30 +0900

    Title: 양수 직사각형의 최대 크기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241009/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 241009/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -2,7 +2,7 @@ n, m = map(int, input().split())
 
 grid = [list(map(int,input().split())) for _ in range(n)]
 
-max_size = 0
+max_size = -1
 
 # 모든 좌표에 대해서 
 for i in range(n):
@@ -10,18 +10,22 @@ for i in range(n):
         if grid[i][j] > 0:
             # 모든 사이즈별로 점검
             directions = [(0,1), (1,0), (0,-1),(-1,0)]
-            for r in range(1,n+1):
-                for c in range(1, m+1):
+            for r in range(n):
+                for c in range(m):
                     length = [r, c, r, c]
                     y, x = i, j
                     for (dy, dx), k in zip(directions, length):
-                        ny, nx = y + dy*k, x + dx*k
-                        if (0<= ny <n and 0 <= nx < m) and grid[ny][nx] > 0:
-                            y, x = ny, nx
-                        else:
-                            length = [0,0,0,0]
+                        for _ in range(k):
+                            ny, nx = y + dy, x + dx
+                            if (0<= ny <n and 0 <= nx < m) and grid[ny][nx] > 0:
+                                y, x = ny, nx
+                            else:
+                                length = [-1,-1,-1,-1]
+                                break
+                        if length[0] == -1:
                             break
-                    size = (length[0]-1) * (length[1]-1)
+                    size = (length[0]+1) * (length[1]+1)
                     max_size = max(max_size, size)
+                    #print(i, j, r, c, size, "max_size", max_size)
 
-print(0 if max_size == 0 else max_size)
+print(max_size if max_size != 0 else -1)
