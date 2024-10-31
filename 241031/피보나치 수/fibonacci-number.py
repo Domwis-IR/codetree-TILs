dcab5a42ff4817f19af8fb43f9e06e3077d5bebd
류지혜/Jihye Ryu <51522587+Domwis-IR@users.noreply.github.com>
2024-10-31 23:47:02 +0900

    Title: 피보나치 수, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241031/피보나치 수/fibonacci-number.py
+++ 241031/피보나치 수/fibonacci-number.py
@@ -1,8 +1,20 @@
+# def fibo(n):
+#     if n == 1 or n == 2:
+#         return 1
+#     else:
+#         return fibo(n-1) + fibo(n-2)
+
 def fibo(n):
-    if n == 1 or n == 2:
-        return 1
+    dp = [-1] * (n+1)
+    # print(dp)
+    if dp[n] != -1:
+        return dp[n]
+    if n <= 2:
+        dp[n] = 1
     else:
-        return fibo(n-1) + fibo(n-2)
+        dp[n] = dp[n-2] + dp[n-1]
+    
+    return dp[n]
 
 n = int(input())
 print(fibo(n))
