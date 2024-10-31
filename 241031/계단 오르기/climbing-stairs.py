30ff926bf4d8fcc7254a2dafa2ffa50ba859456e
류지혜/Jihye Ryu <51522587+Domwis-IR@users.noreply.github.com>
2024-10-31 23:59:20 +0900

    Title: 계단 오르기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241031/계단 오르기/climbing-stairs.py
+++ 241031/계단 오르기/climbing-stairs.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 def up(n):
-    dp = [0] * 1001
+    dp = [-1] * 1001
     dp[2] = 1
     dp[3] = 1
     dp[4] = 1
@@ -9,4 +9,8 @@ def up(n):
         dp[n] = dp[n-2] + dp[n-3]
     return dp[n]
 
-print(up(n))
+answer = up(n)
+if answer == -1:
+    print(0)
+else:
+    print(answer % 10007)
